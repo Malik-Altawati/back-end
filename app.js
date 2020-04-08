90b78db898c87e662601d2ff7d435786be358927
Malik-Altawati <MalikAltawati@gmail.com>
2020-04-08 16:19:32 +0200

    authentication complete

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const fs = require("fs");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+//
+const User = require("./models/User");
+const Token = require("./models/Token");
+//
 
 //Database
 const db = require("./config/database");
 // test db
 db.authenticate()
   .then(() => console.log("db connected..."))
-  .catch(err => console.log(`ERROR### ${err}`));
+  .catch((err) => console.log(`ERROR### ${err}`));
 
 const app = express();
 // app.use(express.static(__dirname + "/public"));
@@ -17,7 +23,53 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("hi"));
+app.get("/", authenticateToken, (req, res) => res.send("authenticated"));
+
+function authenticateToken(req, res, next) {
+  var authHeader = req.headers["authorization"];
+  var token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "1m",
+  });
+}
+
+app.post("/token", (req, res) => {
+  var refreshToken = req.body.token;
+  if (refreshToken == null)
+    return res
+      .status(200)
+      .json({ status: false, message: "no refresh token received" });
+
+  Token.findOne({ where: { refreshToken } })
+    .then((user) => {
+      if (!user) {
+        return res
+          .status(200)
+          .json({ status: false, message: "invalid refreshtoken" });
+      }
+      jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET,
+        (err, user) => {
+          if (err) return res.status(200).json({ status: false, message: err });
+          const accessToken = generateAccessToken({ name: user.username });
+          res.status(200).json({ status: true, accessToken });
+        }
+      );
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 // user routes
 app.use("/user", require("./routes/user"));
--- routes/user.js
+++ routes/user.js
@@ -3,17 +3,19 @@ const router = express.Router();
 const User = require("../models/User");
 const Token = require("../models/Token");
 const Profile = require("../models/Profile");
-
+require("dotenv").config();
 const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 ///// get all users
 router.get("/", (req, res) =>
   User.findAll()
-    .then(users => {
+    .then((users) => {
       res.status(200).send(users);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       //  res.sendStatus(503)
     })
@@ -22,10 +24,10 @@ router.get("/", (req, res) =>
 ///// create a new user
 router.post("/", (req, res) =>
   User.create(req.body)
-    .then(user => {
+    .then((user) => {
       res.status(200).send(user);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       //  res.sendStatus(503)
     })
@@ -35,10 +37,10 @@ router.post("/", (req, res) =>
 router.get("/search", (req, res) => {
   const { name } = req.query;
   User.findAll({ where: { username: { [Op.like]: "%" + name + "%" } } })
-    .then(users => {
+    .then((users) => {
       res.status(200).send(users);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       //  res.sendStatus(503)
     });
@@ -48,13 +50,13 @@ router.get("/search", (req, res) => {
 router.get("/delete", (req, res) => {
   const { id } = req.query;
   User.destroy({ where: { id: id } })
-    .then(data => {
+    .then((data) => {
       if (data === 1) {
         console.log("Deleted successfully");
       }
       res.sendStatus(200);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       // res.sendStatus(503)
     });
@@ -64,15 +66,163 @@ router.get("/delete", (req, res) => {
 router.post("/update", (req, res) => {
   const { id, email, username } = req.body;
   User.update({ email, username }, { where: { id } })
-    .then(data => {
+    .then((data) => {
       if (data === 1) {
         console.log("Updated successfully");
       }
       res.sendStatus(200);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       // res.sendStatus(503)
+    });
+});
+
+/*
+───────────────────────────────────────────────────────────────────────────────────────────────
+─██████████████─██████████─██████████████─██████──────────██████─██████──██████─██████████████─
+─██░░░░░░░░░░██─██░░░░░░██─██░░░░░░░░░░██─██░░██████████──██░░██─██░░██──██░░██─██░░░░░░░░░░██─
+─██░░██████████─████░░████─██░░██████████─██░░░░░░░░░░██──██░░██─██░░██──██░░██─██░░██████░░██─
+─██░░██───────────██░░██───██░░██─────────██░░██████░░██──██░░██─██░░██──██░░██─██░░██──██░░██─
+─██░░██████████───██░░██───██░░██─────────██░░██──██░░██──██░░██─██░░██──██░░██─██░░██████░░██─
+─██░░░░░░░░░░██───██░░██───██░░██──██████─██░░██──██░░██──██░░██─██░░██──██░░██─██░░░░░░░░░░██─
+─██████████░░██───██░░██───██░░██──██░░██─██░░██──██░░██──██░░██─██░░██──██░░██─██░░██████████─
+─────────██░░██───██░░██───██░░██──██░░██─██░░██──██░░██████░░██─██░░██──██░░██─██░░██─────────
+─██████████░░██─████░░████─██░░██████░░██─██░░██──██░░░░░░░░░░██─██░░██████░░██─██░░██─────────
+─██░░░░░░░░░░██─██░░░░░░██─██░░░░░░░░░░██─██░░██──██████████░░██─██░░░░░░░░░░██─██░░██─────────
+─██████████████─██████████─██████████████─██████──────────██████─██████████████─██████─────────
+───────────────────────────────────────────────────────────────────────────────────────────────
+*/
+
+router.post("/signup", (req, res) => {
+  var { password, email, username } = req.body;
+  User.findOne({
+    where: {
+      [Op.or]: [{ email: email }, { username: username }],
+    },
+  })
+    .then((users) => {
+      if (users) {
+        return res
+          .status(200)
+          .json({ status: false, message: "user or email is used" });
+      } else {
+        bcrypt.hash(password, 10, function (err, hash) {
+          if (err) throw err;
+          password = hash;
+          User.create({ username, email, password })
+            .then((user) => {
+              res.status(200).json({ status: true, message: user });
+            })
+            .catch((err) => {
+              console.log(err);
+              res.sendStatus(503);
+            });
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+/*
+────────────────────────────────────────────────────────────────────────────────
+─██████─────────██████████████─██████████████─██████████─██████──────────██████─
+─██░░██─────────██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░██─██░░██████████──██░░██─
+─██░░██─────────██░░██████░░██─██░░██████████─████░░████─██░░░░░░░░░░██──██░░██─
+─██░░██─────────██░░██──██░░██─██░░██───────────██░░██───██░░██████░░██──██░░██─
+─██░░██─────────██░░██──██░░██─██░░██───────────██░░██───██░░██──██░░██──██░░██─
+─██░░██─────────██░░██──██░░██─██░░██──██████───██░░██───██░░██──██░░██──██░░██─
+─██░░██─────────██░░██──██░░██─██░░██──██░░██───██░░██───██░░██──██░░██──██░░██─
+─██░░██─────────██░░██──██░░██─██░░██──██░░██───██░░██───██░░██──██░░██████░░██─
+─██░░██████████─██░░██████░░██─██░░██████░░██─████░░████─██░░██──██░░░░░░░░░░██─
+─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░██─██░░██──██████████░░██─
+─██████████████─██████████████─██████████████─██████████─██████──────────██████─
+────────────────────────────────────────────────────────────────────────────────
+*/
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "30m",
+  });
+}
+
+router.post("/login", (req, res) => {
+  // takes name which can be username or email and a password
+  var { password, name } = req.body;
+  User.findOne({
+    where: {
+      [Op.or]: [{ email: name }, { username: name }],
+    },
+  }).then((user) => {
+    if (user) {
+      bcrypt.compare(password, user.password, function (err, result) {
+        if (result) {
+          var payload = { name: user.username };
+          const accessToken = generateAccessToken(payload);
+          const refreshToken = jwt.sign(
+            payload,
+            process.env.REFRESH_TOKEN_SECRET
+          );
+          var data = { user_id: user.id, refreshToken };
+          Token.create(data)
+            .then((token) => {
+              res.status(200).json({
+                status: true,
+                message: user,
+                accessToken,
+                refreshToken,
+              });
+            })
+            .catch((err) => {
+              console.log(err)
+            });
+        } else {
+          res
+            .status(200)
+            .json({ status: false, message: "incorrect password" });
+        }
+      });
+    } else {
+      res.status(200).json({ status: false, message: "invalid user" });
+    }
+  });
+});
+
+/*
+
+───────────────────────────────────────────────────────────────────────────────────────────
+─██████─────────██████████████─██████████████─██████████████─██████──██████─██████████████─
+─██░░██─────────██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░██──██░░██─██░░░░░░░░░░██─
+─██░░██─────────██░░██████░░██─██░░██████████─██░░██████░░██─██░░██──██░░██─██████░░██████─
+─██░░██─────────██░░██──██░░██─██░░██─────────██░░██──██░░██─██░░██──██░░██─────██░░██─────
+─██░░██─────────██░░██──██░░██─██░░██─────────██░░██──██░░██─██░░██──██░░██─────██░░██─────
+─██░░██─────────██░░██──██░░██─██░░██──██████─██░░██──██░░██─██░░██──██░░██─────██░░██─────
+─██░░██─────────██░░██──██░░██─██░░██──██░░██─██░░██──██░░██─██░░██──██░░██─────██░░██─────
+─██░░██─────────██░░██──██░░██─██░░██──██░░██─██░░██──██░░██─██░░██──██░░██─────██░░██─────
+─██░░██████████─██░░██████░░██─██░░██████░░██─██░░██████░░██─██░░██████░░██─────██░░██─────
+─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─────██░░██─────
+─██████████████─██████████████─██████████████─██████████████─██████████████─────██████─────
+───────────────────────────────────────────────────────────────────────────────────────────
+*/
+router.post("/logout", (req, res) => {
+  var user_id = req.body.user_id;
+  Token.destroy({ where: { user_id } })
+    .then((data) => {
+      if (data > 0) {
+        res.status(200).json({
+          status: true,
+          message: "Refresh Token Deleted successfully",
+        });
+      }
+      res.status(200).json({
+        status: true,
+        message: "No refreshToken with that user_Id",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
 });
 
