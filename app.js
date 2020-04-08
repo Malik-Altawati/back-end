6a31f4493b70bf2884697715f7f66b1bb962d43f
Malik-Altawati <MalikAltawati@gmail.com>
2020-04-08 17:43:41 +0200

    token duration updated

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ function authenticateToken(req, res, next) {
 
 function generateAccessToken(user) {
   return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "1m",
+    expiresIn: "30m",
   });
 }
 
